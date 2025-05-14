3a9dab8d34ad0a20d6fa23fec435b87206a84e0d
kwzeqt <kajibelleza@gmail.com>
2025-05-14 11:55:29 +0400

    Updated index.ts

--- app/.vitepress/theme/index.ts
+++ app/.vitepress/theme/index.ts
@@ -3,13 +3,13 @@ import { h } from 'vue';
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import './style.css';
-import Analytics from '@vercel/analytics';
+import { inject } from '@vercel/analytics';
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      default: () => h(Analytics),
+      default: () => h(inject),
     });
   },
   enhanceApp({ app, router, siteData }) {},
