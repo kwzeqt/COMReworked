fbc7629c62e993b8ba6f3001583e2c5df876e9c4
kwzeqt <kajibelleza@gmail.com>
2025-05-14 21:39:37 +0400

    Used react for analytics

--- app/.vitepress/theme/index.ts
+++ app/.vitepress/theme/index.ts
@@ -1,15 +1,14 @@
-// https://vitepress.dev/guide/custom-theme
 import { h } from 'vue';
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import './style.css';
-import inject from '@vercel/analytics';
+import { Analytics } from '@vercel/analytics/react';
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      default: () => h(inject),
+      default: () => h(Analytics),
     });
   },
   enhanceApp({ app, router, siteData }) {},
