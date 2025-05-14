008c57cd9c0b9d6cc24da204f15e3ec801cd5e6f
kwzeqt <kajibelleza@gmail.com>
2025-05-14 11:51:19 +0400

    Added vercel analytics

--- app/.vitepress/theme/index.ts
+++ app/.vitepress/theme/index.ts
@@ -3,12 +3,13 @@ import { h } from 'vue';
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import './style.css';
+import Analytics from '@vercel/analytics';
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      default: () => h(Analytics),
     });
   },
   enhanceApp({ app, router, siteData }) {},
