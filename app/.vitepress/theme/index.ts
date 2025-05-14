9e71da1984fb765ca8e479f23e5140331e9431b9
kwzeqt <kajibelleza@gmail.com>
2025-05-14 21:16:59 +0400

    Fixed vercel analytics issues

--- app/.vitepress/theme/index.ts
+++ app/.vitepress/theme/index.ts
@@ -3,7 +3,7 @@ import { h } from 'vue';
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import './style.css';
-import { inject } from '@vercel/analytics';
+import inject from '@vercel/analytics';
 
 export default {
   extends: DefaultTheme,
